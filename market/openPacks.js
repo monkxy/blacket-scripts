bf30e196676e5b49584130957ec3cb289e7a5f7c
monkey <monkxy@monkxy.org>
2023-05-18 15:39:13 -0500

    log blooks obtained, loop on error

--- market/openPacks.js
+++ market/openPacks.js
@@ -1,52 +1,83 @@
 (async () => {
     if (!blacket.packs) {
-        return alert('You must be on the Market to run this script.')
+        return alert('You must be on the Market to run this script.');
     }
-    let selection = prompt('Which pack would you like to open?\n\nOpenable Packs:\n' + Object.keys(blacket.packs).join('\n'))
-    if (!blacket.packs[selection]) {
-        return alert("That pack was not found.")
+    let selection = '';
+    while (!blacket.packs[selection]) {
+        selection = prompt(
+            'Which pack would you like to open?\n\nOpenable Packs:\n' +
+            Object.keys(blacket.packs).join('\n')
+        );
+        selection = Object.keys(blacket.packs).find(
+            (pack) => pack.toLowerCase() === selection.toLowerCase()
+        );
+        if (!selection) {
+            alert('That pack was not found.');
+        }
     }
-    let amount = prompt('How many packs do you want to open?\n\nYou can also use * to open as many as you can purchase.')
-    if (amount.toString() === '*') {
-        amount = Math.floor(blacket.user.tokens / blacket.packs[selection].price)
+    let amount = '';
+    while (isNaN(amount) || amount < 0 || amount < 1 || amount * blacket.packs[selection].price > blacket.user.tokens) {
+        amount = prompt(
+            'How many packs do you want to open?\n\nYou can also use * to open as many as you can purchase.'
+        );
+        if (amount.toString() === '*') {
+            amount = Math.floor(blacket.user.tokens / blacket.packs[selection].price);
+        }
+        if (isNaN(amount) || amount < 0) {
+            alert('That is not a valid amount');
+        } else if (amount < 1 || amount * blacket.packs[selection].price > blacket.user.tokens) {
+            alert('You cannot afford that many packs');
+        }
     }
-    if (isNaN(amount) || amount < 0) {
-        return alert('That is not a valid amount')
+    let speed = '';
+    while (isNaN(speed) || speed < 125) {
+        speed = Number.parseInt(
+            prompt(
+                'What speed would you like to open packs at? This is in milliseconds.\n\nThe lowest you can go is 125ms.'
+            )
+        );
+        if (isNaN(speed)) {
+            alert('That is not a valid speed');
+        } else if (speed < 125) {
+            alert('If you run the speed any lower, you will be instantly blacklisted.');
+        }
     }
-    if (amount < 1 || amount * blacket.packs[selection].price > blacket.user.tokens) {
-        return alert('You cannot afford that many packs')
-    }
-    let speed = Number.parseInt(prompt('What speed would you like to open packs at? This is in milliseconds.\n\nThe lowest you can go is 125ms.'))
-    if (isNaN(speed)) {
-        return alert('That is not a valid speed')
-    }
-    if (speed < 125) {
-        return alert('If you run the speed any lower, you will be instantly blacklisted.')
-    }
-    window.blooks = []
-    let opened = 0
+    window.blooks = [];
+    let opened = 0;
     async function openPack(pack) {
         const data = {
             pack: pack
-        }
+        };
         await blacket.requests.post('/worker/open', data, (res) => {
             if (res.error) {
-                return
+                return;
             }
-            opened++
-            blooks.push(res.blook)
-        })
+            opened++;
+            console.log(
+                '%c' +
+                res.blook +
+                ' (' +
+                opened +
+                '/' +
+                amount +
+                ')',
+                'font-size: 20px; color: white; text-shadow: 0px 0px 15px ' +
+                blacket.rarities[blacket.blooks[res.blook].rarity].color +
+                '; font-family: monospace;'
+            );
+            blooks.push(res.blook);
+        });
     }
     let interval = setInterval(() => {
         if (opened < amount) {
-            openPack(selection)
+            openPack(selection);
         } else {
-            clearInterval(interval)
-            let results = {}
+            clearInterval(interval);
+            let results = {};
             blooks.forEach((blook) => {
-                results[blook] = (results[blook] || 0) + 1
-            })
+                results[blook] = (results[blook] || 0) + 1;
+            });
             alert('Blooks Obtained:\n' + Object.entries(results).map((entry) => '    ' + entry[1] + ' ' + entry[0]).join('\n'))
         }
-    }, speed)
+    }, speed);
 })();
