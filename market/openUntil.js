829460ee83f299a163cefb542af7ec60dbab7a96
monkxy <monkxy@monkxy.org>
2023-12-28 23:17:07 -0600

    fixed

--- market/openUntil.js
+++ market/openUntil.js
@@ -1,6 +1,6 @@
 (async () => {
     if (!blacket.packs) {
-        return alert('You must be on the Market to run this script.')
+        return alert("You must be on the Market to run this script.")
     }
     let blooksInPacks = {}
     Object.keys(blacket.packs).forEach((pack) => {
@@ -8,27 +8,27 @@
             blooksInPacks[blook] = pack
         })
     });
-    let targetBlook = prompt('Which Blook would you like to target?\n\nAll Blooks:\n' + Object.keys(blacket.blooks).join('\n'))
+    let targetBlook = prompt("Which Blook would you like to target?\n\nAll Blooks:\n" + Object.keys(blacket.blooks).join("\n"))
     if (!blacket.blooks[targetBlook]) {
         return alert("That Blook doesn't exist.")
     }
     let targetPack = blooksInPacks[targetBlook],
-        limit = prompt('How many packs do you want to open before stopping?\n\nYou can also use * to open as many as you can purchase.')
-    if (limit.toString() === '*') {
+        limit = prompt("How many packs do you want to open before stopping?\n\nYou can also use * to open as many as you can purchase.")
+    if (limit.toString() === "*") {
         limit = Math.floor(blacket.user.tokens / blacket.packs[targetPack].price)
     }
     if (isNaN(limit) || limit < 0) {
-        return alert('That is not a valid limit.')
+        return alert("That is not a valid limit.")
     }
     if (limit < 1 || limit * blacket.packs[targetPack].price > blacket.user.tokens) {
-        return alert('You cannot afford that many packs.')
+        return alert("You cannot afford that many packs.")
     }
-    let speed = Number.parseInt(prompt('What speed would you like to open packs at? This is in milliseconds.\n\nThe lowest you can go is 125ms.'))
+    let speed = Number.parseInt(prompt("What speed would you like to open packs at? This is in milliseconds.\n\nThe lowest you can go is 125ms."))
     if (isNaN(speed)) {
-        return alert('That is not a valid speed.')
+        return alert("That is not a valid speed.")
     }
     if (speed < 125) {
-        return alert('If you run the speed any lower, you will be instantly blacklisted.')
+        return alert("If you run the speed any lower, you will be instantly blacklisted.")
     }
     window.blooks = [];
     let opened = 0,
@@ -38,7 +38,7 @@
         const data = {
             pack: pack
         };
-        blacket.requests.post('/worker/open', data, (res) => {
+        blacket.requests.post("/worker2/open", data, (res) => {
             if (res.error) {
                 return;
             }
@@ -59,9 +59,9 @@
                 results[blook] = (results[blook] || 0) + 1;
             });
             if (found) {
-                alert('Unlocked ' + targetBlook + ' in ' + opened + ' packs.\n\nOther Blooks Obtained:\n' + Object.entries(results).map((entry) => '    ' + entry[1] + ' ' + entry[0]).join('\n'))
+                alert("Unlocked " + targetBlook + " in " + opened + " packs.\n\nOther Blooks Obtained:\n" + Object.entries(results).map((entry) => "    " + entry[1] + " " + entry[0]).join("\n"))
             } else {
-                alert('I could not unlock ' + targetBlook + ' in ' + opened + ' packs.\nObtained:\n' + Object.entries(results).map((entry) => '    ' + entry[1] + ' ' + entry[0]).join('\n'))
+                alert("I could not unlock " + targetBlook + " in " + opened + " packs.\nObtained:\n" + Object.entries(results).map((entry) => "    " + entry[1] + " " + entry[0]).join("\n"))
             }
         }
     }, speed)
